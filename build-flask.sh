e2c8571ecf080588c15c893c191c979632a8ef4b
cmndcntrl <sorengr@wwt.com>
2023-12-05 11:17:07 -0600

    update

--- build-flask.sh
+++ build-flask.sh
@@ -16,7 +16,7 @@ cd $name;
 
 sed -i 's/ztsc/$name/g' 000-default.conf
 sudo a2ensite 000-default.conf
-curl "https://raw.githubusercontent.com/cmndcntrl/auto/main/prep-flask-web.py" | python3
 virutalenv -p python3 $name # $name here begins representing the virutal environment
+curl "https://raw.githubusercontent.com/cmndcntrl/auto/main/prep-flask-web.py" | python3
 source $name/bin/activate
 export FLASK_APP=$name.py # $name is the flask app name in this line
--- vuln-dev-db.sh
+++ vuln-dev-db.sh
@@ -3,4 +3,3 @@ sudo useradd dev && echo 'dev:P05tGr35D3v!' | sudo chpasswd && sudo usermod -aG 
 apt-get install postgresql -y
 
 sudo -u postgres psql
-
